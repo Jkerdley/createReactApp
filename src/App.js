f9c6932ef9b489a8c1e8e669a2c9e8743f85479a
Jkerdley <jk-erley@yandex.ru>
2024-08-21 19:30:56 +0700

    with JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
+// declarative style
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// declarative + imperative style
+export const App = () => {
+	// declarative style
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload new app
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+				{/* imperative style */}
+				<p> Текущий год: {new Date().getFullYear()}</p>
+			</header>
+		</div>
+	);
+};
