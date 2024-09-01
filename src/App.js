2cc9e9f32139ea780f0c43d81585cc126bc1d7ac
Jkerdley <jk-erley@yandex.ru>
2024-09-01 15:46:25 +0700

    new app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const elements = [
-	{ type: 'img', props: { src: logo, className: 'App-logo', alt: 'logo' } },
-	{ type: 'p', props: { children: 'Редактируйте ' } },
-	{ type: 'code', props: { children: 'src/App.js' } },
-	{ type: 'p', props: { children: ' и сохраните, чтобы перезапустить новое приложение' } },
-	{
-		type: 'a',
-		props: {
-			className: 'App-link',
-			href: 'https://reactjs.org',
-			target: '_blank',
-			rel: 'noopener noreferrer',
-			children: 'Узнайте о React',
-		},
-	},
-	{ type: 'p', props: { children: `Текущий год: ${new Date().getFullYear()}` } },
-];
+import React, { useState } from 'react';
+import styles from './App.module.css';
 
 export const App = () => {
-	const header = React.createElement(
-		'header',
-		{ className: 'App-header' },
-		elements.map((element) => React.createElement(element.type, element.props)),
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
+	const isValueValid = value.length >= 3;
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt('Введите значение');
+		if (promptValue === null || promptValue === undefined) return;
+
+		if (promptValue.length < 3) {
+			setError('Введите минимум 3 символа');
+		} else {
+			setValue(promptValue);
+			setError('');
+		}
+	};
+
+	const onAddButtonClick = () => {
+		if (isValueValid) {
+			const date = new Date();
+			const valueDate = `(${date.toLocaleDateString()} ${date.toLocaleTimeString()})`;
+			const updatedList = [...list, { id: Date.now(), value, valueDate }];
+			setList(updatedList);
+			setValue('');
+			setError('');
+		}
+	};
+
+	return (
+		<div className={styles.app}>
+			<h1 className={styles.pageHeading}>Ввод значения</h1>
+			<p className={styles.noMarginText}>
+				Текущее значение <code>value</code>: <output className={styles.currentValue}>{value}</output>
+			</p>
+			{error !== '' && <div className={styles.error}>{error}</div>}
+			<div className={styles.buttonsContainer}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button className={styles.button} disabled={!isValueValid} onClick={onAddButtonClick}>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles.listContainer}>
+				<h2 className={styles.listHeading}>Список:</h2>
+				{list.length === 0 ? (
+					<p className={styles.noMarginText}>Нет добавленных элементов</p>
+				) : (
+					<ul className={styles.list}>
+						{list.map((item) => (
+							<li key={item.id} className={styles.listItem}>
+								{item.value} {item.valueDate}
+							</li>
+						))}
+					</ul>
+				)}
+			</div>
+		</div>
 	);
-	return React.createElement('div', { className: 'App' }, header);
 };
